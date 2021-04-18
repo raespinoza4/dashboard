b41bb350c57328b3fa0b91b472cdddd2f7cfecbc
Rodrigo <respinoza@floid.cl>
2021-04-17 23:16:38 -0400

    :sparkles: Custom error handler and middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const errorResponse = require('../utils/errorResponse');
+const ErorrResponse = require('../utils/errorResponse');
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,10 +16,7 @@ exports.register = async (req, res, next) => {
       user,
     });
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message,
-    });
+    next(error);
   }
 };
 
@@ -25,22 +24,20 @@ exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res
-      .status(400)
-      .json({ success: false, error: 'Please provide email and password' });
+    return next(new errorResponse('Please provide an email and password', 400));
   }
 
   try {
     const user = await User.findOne({ email }).select('+password');
 
     if (!user) {
-      res.status(404).json({ success: false, error: 'Invalid credentials' });
+      return next(new errorResponse('Invalid credentials', 401));
     }
 
     const isMatch = await user.matchPassword(password);
 
     if (!isMatch) {
-      res.status(404).json({ success: false, error: 'Invalid credentials' });
+      return next(new errorResponse('Invalid credentials', 401));
     }
 
     res.status(200).json({
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
 const connectDB = require('./config/db');
+const errorHandler = require('./middleware/error');
 
 // Connect DB
 connectDB();
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth/', require('./routes/auth'));
+
+// Error Handler (must be the last middleware)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
